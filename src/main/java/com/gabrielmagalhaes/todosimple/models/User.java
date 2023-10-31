048a8d36ff1e90c473f689333061367425d39203
Gabrielmc35 <Gabrielmc35@hotmail.com>
2023-10-31 13:40:58 -0300

    extention recommendation  + Create User model

---
+++ src/main/java/com/gabrielmagalhaes/todosimple/models/User.java
@@ -0,0 +1,130 @@
+package com.gabrielmagalhaes.todosimple.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+
+    public interface CreateUser {
+    
+
+        
+    }
+
+    public interface UpdateUser {
+
+
+    }
+    
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "username", nullable = false, unique = true)
+    @NotNull (groups = CreateUser.class)
+    @NotEmpty (groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min= 2, max = 100)
+    private String username;
+
+    @Column(name = "password",nullable = false)
+    @NotNull (groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty (groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class,UpdateUser.class}, min =6, max = 35)
+    private String password;
+
+
+   // private Lst<Task> tasks = new ArrayList<Task> ();
+
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public User username(String username) {
+        setUsername(username);
+        return this;
+    }
+
+    public User password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", username='" + getUsername() + "'" +
+            ", password='" + getPassword() + "'" +
+            "}";
+    }
+   
+}
