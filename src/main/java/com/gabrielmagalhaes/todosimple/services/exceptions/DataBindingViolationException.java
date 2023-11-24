a91739a0ca3cdf421a898fdd28b64a64547b28b9
Gabrielmc35 <Gabrielmc35@hotmail.com>
2023-11-24 16:01:15 -0300

    Custom exception

---
+++ src/main/java/com/gabrielmagalhaes/todosimple/services/exceptions/DataBindingViolationException.java
@@ -0,0 +1,20 @@
+package com.gabrielmagalhaes.todosimple.services.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class DataBindingViolationException extends DataIntegrityViolationException {
+    
+public DataBindingViolationException(String message)
+{
+    super(message);
+}
+
+}
